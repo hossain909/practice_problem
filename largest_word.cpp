5f77d4c3b7631f256dce85d06851ce005961cbe8
Taieb Hossain <hossain@Taiebs-MacBook-Pro.local>
2023-05-31 04:08:13 +0600

    clean up file

--- largest_word.cpp
+++ largest_word.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main()
 {
-	string str = "I love Bangladesh";
-	int len = str.length();
-	int currLen = 0, maxLen = 0;
-	int st = 0, maxSt = 0;
-	for (int i = 0; i < len; i++) {
-		if (str[i] != ' ') {
-			currLen++;
-			
-		}
-		else {
-			maxLen = max(currLen, maxLen);
-			currLen = 0;
-			
-		}
-	}
-	cout << max(currLen, maxLen) << endl;
-	
-	return 0;
+  string str = "I love Bangladesh";
+  int len = str.length();
+  int currLen = 0, maxLen = 0;
+  int st = 0, maxSt = 0;
+  for (int i = 0; i < len; i++) {
+    if (str[i] != ' ') {
+      currLen++;
+      
+    }
+    else {
+      maxLen = max(currLen, maxLen);
+      currLen = 0;
+      
+    }
+  }
+  cout << max(currLen, maxLen) << endl;
+  
+  return 0;
 }
