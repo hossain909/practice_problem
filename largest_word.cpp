38999a9e8ac50b73a4f6a5a33a0ed6ccf8342af6
Taieb Hossain <hossain@Taiebs-MacBook-Pro.local>
2023-05-31 03:37:33 +0600

    practice problem

---
+++ largest_word.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string str = "I love Bangladesh";
+	int len = str.length();
+	int currLen = 0, maxLen = 0;
+	int st = 0, maxSt = 0;
+	for (int i = 0; i < len; i++) {
+		if (str[i] != ' ') {
+			currLen++;
+			
+		}
+		else {
+			maxLen = max(currLen, maxLen);
+			currLen = 0;
+			
+		}
+	}
+	cout << max(currLen, maxLen) << endl;
+	
+	return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// check pallindrome
+	int n;
+	cin >> n;
+	char arr[n + 1];
+	cin >> arr;
+	
+	int i = 0;
+	bool check = true;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] != arr[n - 1 - i]) {
+			check = false;
+			break;
+		}
+	}
+	if (check == true) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	
+	
+	return 0;
+}
