de8b5764fe87ef7b863c43792c855c70ae33ad95
Taieb Hossain <hossain@Taiebs-MacBook-Pro.local>
2023-05-31 04:11:42 +0600

    resolved indentation

--- pallindrome.cpp
+++ pallindrome.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main()
 {
-	// check pallindrome
-	int n;
-	cin >> n;
-	char arr[n + 1];
-	cin >> arr;
-	
-	int i = 0;
-	bool check = true;
-	for (int i = 0; i < n; i++) {
-		if (arr[i] != arr[n - 1 - i]) {
-			check = false;
-			break;
-		}
-	}
-	if (check == true) cout << "YES" << endl;
-	else cout << "NO" << endl;
-	
-	
-	return 0;
+  // check pallindrome
+  int n;
+  cin >> n;
+  char arr[n + 1];
+  cin >> arr;
+  
+  int i = 0;
+  bool check = true;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] != arr[n - 1 - i]) {
+      check = false;
+      break;
+    }
+  }
+  if (check == true) cout << "YES" << endl;
+  else cout << "NO" << endl;
+  
+  
+  return 0;
 }
